51fb2c91f0a5dba5be7f9c54ad1dae5e4e627a18
NATAKENSHI DEVELOPMENT <ptazzamcodex@gmail.com>
2025-05-29 17:39:54 +0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AzzamDev'
+description 'DanceRoom By Natakenshi'
+version '1.0.0'
+
+server_script 'server.lua'
+client_script 'client.lua'
+
+shared_script '@es_extended/imports.lua'
+
+dependencies {
+    'es_extended'
+}
